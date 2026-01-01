73921dbb672fd784168dd19c27352c9bb6946094
prasaddeshmukh6969-eng <prasad.deshmukh6969@gmail.com>
2026-01-02 00:06:26 +0530

    Final cleanup: removed 25+ unnecessary files, lean deployment-ready codebase

--- core/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Core module exports"""
-from .config import settings, get_voice_config, get_language_voice
-
-__all__ = ['settings', 'get_voice_config', 'get_language_voice']
--- routers/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Routers module exports"""
-from . import v1_generation
-
-__all__ = ['v1_generation']
